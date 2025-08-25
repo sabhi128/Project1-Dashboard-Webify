1d169bfd3068918bd2e256e27d86a5a39cba6aff
sabhi128 <sabhiahm12@gmail.com>
2025-08-25 17:41:08 +0500

    Update vite.config.js
    merge conflicts resolved

--- webify-dashboard/vite.config.js
+++ webify-dashboard/vite.config.js
@@ -1,20 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-<<<<<<< HEAD
-=======
-
->>>>>>> upstream/main
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   plugins: [
     react(),
     tailwindcss(),
   ],
-=======
-  plugins: [tailwindcss(),
-react()],
->>>>>>> upstream/main
 })
